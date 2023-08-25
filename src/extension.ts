1bb42a81a0347404a9a0a01900b71fbbfda61316
ali <alioliaee31@gmail.com>
2023-08-25 11:58:11 +0330

    add runner command

--- src/extension.ts
+++ src/extension.ts
@@ -13,7 +13,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('rn-style-generator.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('rn-style-generator.gen-styles', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from rn-style-generator!');
